0a6f9204c846974c2e9ec1e41cebed268139d14e
scaldas <sebastian.caldas@gmail.com>
2015-04-05 12:36:29 -0500

    Correcciones Punto 1a, todavia quedan dudas

--- Punto1/a.py
+++ Punto1/a.py
@@ -38,10 +38,12 @@ delta_z = (max(z_coord) - min_z)/size
 #print("dy = %f" % (delta_y))
 #print("dz = %f" % (delta_z))
 
-#Construimos rho_gorrito
-rho_gorrito = np.zeros((size, size, size))
+print("Inicializa la matriz")
+#Construimos rho
+rho = np.zeros((size, size, size))
 num_puntos = np.zeros((size, size, size))
 
+print("Comienza a construir rho")
 for i in range(0,len(x_coord)):
 	x = x_coord[i]
 	y = y_coord[i]
@@ -51,28 +53,62 @@ for i in range(0,len(x_coord)):
 	j = int((y-min_y)/delta_y)
 	k = int((z-min_z)/delta_z)
 
-	if(i == 1000):
-		i = 999
-	if(j == 1000):
-		j = 999
-	if(k == 1000):
-		k = 999
+	if i == size:
+		i = size - 1
+	if j == size:
+		j = size - 1
+	if k == size:
+		k = size - 1
 
-	x_center = i + delta_x/2
-	y_center = j + delta_y/2
-	z_center = k + delta_z/2
-
-	W_x = 1 - (abs(x-x_center)/delta_x)
-	W_y = 1 - (abs(y-y_center)/delta_y)
-	W_z = 1 - (abs(z-z_center)/delta_z)
-
+	#DUDA!
+	#Sigo sin saber cual masa utilizar
+	#Uso solo la de la celda o la que esta fuera tambien?
 	num_puntos[i,j,k] += 1
-	rho_gorrito[i,j,k] += W_x*W_y*W_z
 
-rho_gorrito = num_puntos*rho_gorrito
-rho_gorrito = (1/(delta_x*delta_y*delta_z))*rho_gorrito
+	for a in range(-1,1):
+		for b in range(-1,1):
+			for c in range(-1,1):
 
+				new_i = i + a
+				new_j = j + b
+				new_k = k + c
+				
+				if new_i < size and new_j < size and new_k < size and new_i > -1 and new_j > -1 and new_k > -1:
+					x_center = delta_x*(float(new_i) + 1/2)
+					y_center = delta_y*(float(new_j) + 1/2)
+					z_center = delta_z*(float(new_k) + 1/2)
 
+					if abs(x-x_center) < delta_x:
+						W_x = 1 - (abs(x-x_center)/delta_x)
+					else:
+						W_x = 0
+
+					if abs(y-y_center) < delta_y:
+						W_y = 1 - (abs(y-y_center)/delta_y)
+					else:
+						W_y = 0
+					
+					if abs(z-z_center) < delta_z:
+						W_z = 1 - (abs(z-z_center)/delta_z)
+					else:
+						W_z = 0
+
+					rho[new_i,new_j,new_k] += W_x*W_y*W_z
+
+					#Descomentar esta linea si cada nube cuenta en cada celda
+					#if W_x != 0 and W_y != 0 and W_z != 0:
+					#	num_puntos[new_i,new_j,new_k] += 1
+
+rho = num_puntos*rho
+rho = (1/(delta_x*delta_y*delta_z))*rho
+
+print("FIN")
+
+#NO USAR, puede llegar a crear un archivo de 8 GB
+#print("Inicia Impresion")
+#f = open('resultados.txt', 'w')
+#f.write(rho)
+#f.close
 
 
 
