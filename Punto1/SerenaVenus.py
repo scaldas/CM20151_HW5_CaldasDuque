8531288407cd9ca896b644a551315cb1806f0cee
scaldas <sebastian.caldas@gmail.com>
2015-04-07 17:52:21 -0500

    Fuerzas Calculadas

--- Punto1/SerenaVenus.py
+++ Punto1/SerenaVenus.py
@@ -18,10 +18,13 @@ for line in raw_data:
 	y_coord.append(float(particle[2]))
 	z_coord.append(float(particle[3]))
 
+#Probamos imprimiendo la informacion de las 5 primeras particulas
+#for i in range(0,5):
+#	print("%f, %f, %f" % (x_coord[i], y_coord[i], z_coord[i]))
 
 #Calculamos los delta
 #Cada delta se calcula como (maximo - minimo)/1000
-#Guardamos los minimos pues solo 
+#Guardamos los minimos pues seran de utilidad mas adelante
 size = 100
 min_x = min(x_coord)
 min_y = min(y_coord)
@@ -36,12 +39,11 @@ delta_z = (max(z_coord) - min_z)/size
 #print("dy = %f" % (delta_y))
 #print("dz = %f" % (delta_z))
 
-print("Inicializa la matriz")
-#Construimos rho
+#Inicializamos la matriz
 rho = np.zeros((size, size, size))
 num_puntos = np.zeros((size, size, size))
 
-print("Comienza a construir rho")
+#Construimos rho
 for i in range(0,len(x_coord)):
 	x = x_coord[i]
 	y = y_coord[i]
@@ -51,6 +53,7 @@ for i in range(0,len(x_coord)):
 	j = int((y-min_y)/delta_y)
 	k = int((z-min_z)/delta_z)
 
+	#Las particulas que caigan en los limites superiores de la grilla se cuentan en la ultima celda
 	if i == size:
 		i = size - 1
 	if j == size:
@@ -61,53 +64,55 @@ for i in range(0,len(x_coord)):
 	#DUDA!
 	#Sigo sin saber cual masa utilizar
 	#Uso solo la de la celda o la que esta fuera tambien?
-	num_puntos[i,j,k] += 1
+	#num_puntos[i,j,k] += 1
 
+	#Agregamos la particula no solo a la celda en la que se encuentra sino a las adyacentes donde pudiera ser relevante
+	#El algortimo se hace asi para hacerlo mas eficiente
+	#No es posible que una particula afecte una celda que no sea su vecina. Sin embargo, se deben revisar los vecinos en las tres dimensiones
 	for a in range(-1,1):
 		for b in range(-1,1):
 			for c in range(-1,1):
-
 				new_i = i + a
 				new_j = j + b
 				new_k = k + c
 				
+				#Los limites de la grilla tendran algunos vecinos que esten fuera de la grilla
 				if new_i < size and new_j < size and new_k < size and new_i > -1 and new_j > -1 and new_k > -1:
 					x_center = min_x + delta_x*(float(new_i) + 0.5)
 					y_center = min_y + delta_y*(float(new_j) + 0.5)
 					z_center = min_z + delta_z*(float(new_k) + 0.5)
 
 					if abs(x-x_center) < delta_x:
-						W_x = 1 - (abs(x-x_center)/delta_x)
+						W_x = 1.0 - (abs(x-x_center)/delta_x)
 					else:
-						W_x = 0
+						W_x = 0.0
 
 					if abs(y-y_center) < delta_y:
-						W_y = 1 - (abs(y-y_center)/delta_y)
+						W_y = 1.0 - (abs(y-y_center)/delta_y)
 					else:
-						W_y = 0
+						W_y = 0.0
 					
 					if abs(z-z_center) < delta_z:
-						W_z = 1 - (abs(z-z_center)/delta_z)
+						W_z = 1.0 - (abs(z-z_center)/delta_z)
 					else:
-						W_z = 0
+						W_z = 0.0
 
 					rho[new_i,new_j,new_k] += W_x*W_y*W_z
 
 					#Descomentar esta linea si cada nube cuenta en cada celda
 					#if W_x != 0 and W_y != 0 and W_z != 0:
 					#	num_puntos[new_i,new_j,new_k] += 1
-
-rho = num_puntos*rho
-rho = (1/(delta_x*delta_y*delta_z))*rho
+#rho = num_puntos*rho
+rho = (1.0/(delta_x*delta_y*delta_z))*rho
 
 print("Listo")
 #Imprimimos rho para revisar que no quede en ceros
 #print(rho)
 
 #1b. 
-#Se encuentra la transformada inversa de Fourier de phi_gorrito que sabemos que phi_gorrito = -rho_gorrito. 
-#Para esto se asume que rho_gorrito = rho
-print('Se comienza a construir la matriz phi. Favor esperar y no entrar en pánico')
+#Se encuentra la transformada inversa de Fourier de phi_gorrito
+#Sabemos que phi_gorrito = -rho_gorrito. 
+print('Se comienza a construir la matriz phi. Favor esperar y no entrar en panico')
 rho_gorrito= np.fft.fftn(rho)
 phi_gorrito = rho_gorrito*(-1)
 phi = np.fft.ifftn(phi_gorrito)
@@ -118,12 +123,23 @@ print('Listo')
 
 #1c.
 print('Finalmente se crea la matriz con las fuerzas')
-#La fuerza la calculamos como F = -Grad(U) 
+#La fuerza la calculamos a partir de sus componentes como sqrt(F_x^2+F_y^2+F_z^2)
+#Por ejemplo, la componente en x se calcula como F_x = - dphi/dx
 
-force = np.gradient(phi)
-force = np.asarray(force)
-force = (-1)*force
+F_x = (phi[1:,:,:] - phi[:-1,:,:])[:,:-1,:-1]/delta_x
+F_y = (phi[:,1:,:] - phi[:,:-1,:])[:-1,:,:-1]/delta_y
+F_z = (phi[:,:,1:] - phi[:,:,:-1])[:-1,:-1,:]/delta_z
 
-#Imprimimos la fuerza para cersiorarnos que el arreglo no este vacio
-#print(force)
+F_x = -1*F_x
+F_y = -1*F_y
+F_z = -1*F_z
+
+F = ((F_x**2)+(F_y**2)+(F_z**2))**(0.5)
+
+#Imprimimos la fuerza para cersiorarnos que el arreglo no este vacio ni lleno de unos
+#print(F)
+
+print(F.max())
+print(F.min())
+
 print('Listo calisto!!')
